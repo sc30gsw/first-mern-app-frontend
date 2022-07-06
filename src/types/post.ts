f6a6f01f2e531e5ac3a1fa8a8de457a3e9addb19
sc30gsw <haidoushi875@gmail.com>
2022-07-06 11:47:13 +0900

    ダミーデータの作成

---
+++ src/types/post.ts
@@ -0,0 +1,9 @@
+export type Post = {
+	id: number;
+	desc: string;
+	photo: string;
+	date: string;
+	userId: number;
+	like: number;
+	comment: number;
+};
---
+++ src/types/user.ts
@@ -0,0 +1,5 @@
+export type User = {
+	id: number;
+	profilePicture: string;
+	username: string;
+};
